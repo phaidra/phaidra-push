d4e2127e5cd110fb012426c6330039a90235b778
RastislavHudak <hudak.rastislav@gmail.com>
2016-04-26 15:54:20 +0200

    texts

--- public/js/controllers/frontend.js
+++ public/js/controllers/frontend.js
@@ -1,11 +1,86 @@
-var app = angular.module('frontendApp', ['ngRoute', 'actionControllers' ,'ngAnimate', 'ngSanitize', 'mm.foundation', 'ajoslin.promise-tracker', 'pasvaz.bindonce', 'frontendService', 'dataService']);
+var app = angular.module('frontendApp', ['ngRoute', 'actionControllers' ,'ngAnimate', 'ngSanitize', 'mm.foundation', 'ajoslin.promise-tracker', 'pasvaz.bindonce', 'pascalprecht.translate', 'frontendService', 'dataService']);
+
+var translationsEN = {
+  //VARIABLE_REPLACEMENT: 'Hi {{name}}',
+  DELETE: 'Delete',
+  DELETE_BUTTON: 'Delete',
+  BACK_BUTTON: 'Back',
+  DELETE_TEXT: 'Sie finden hier alle Objekte, die Sie löschen möchten. Bitte beachten Sie, dass nach Betätigung des  Buttons „Löschen“ die Objekte gelöscht sind und nicht wiederhergestellt werden können.',
+  DELETE_SUCCESS: 'Ihre Objekte wurden nun wie gewünscht gelöscht.',
+  HELP: 'Help',
+  CONTACT: 'Contact',
+  TITLE: 'Title',
+  OBJECTS: 'objects',
+  HITS: 'hits',
+  PHAIDRA_MANAGEMENTMODULE: 'Phaidra Managementmodule',
+  BUTTON_LANG_DE: 'Deutsch',
+  BUTTON_LANG_EN: 'English',
+  CONTACT_1: "University of Vienna<br/>Library and Archive Services<br/>Universitätsring 1<br/>A-1010 Vienna<br/>Austria",
+  SERVICEPAGE: "Service page",
+  IMPRESSUM_1: "Publishing information",
+  IMPRESSUM_2: "Disclosure according to Section 25 of the Austrian Media Code – the party responsible for the content of this page is",
+  IMPRESSUM_3: "Phaidra is not responsible for the content of “external” websites.",
+  HELP_1: "The management tool is available to you for the following activities",
+  HELP_2: "1. You can delete objects from PhaidraTemp",
+  HELP_3: "2. You can long-term archive objects from PhaidraTemp in Phaidra",
+  HELP_4: "Please note before deleting objects that these cannot be recovered after deletion. Before you archive items, please learn about the terms of Phaidra: <a target=\"_blank\" href=\"https://phaidra.univie.ac.at/terms_of_use/show_terms_of_use\">https://phaidra.univie.ac.at/terms_of_use/show_terms_of_use</a>",
+  HELP_5: "You can find additional Information about Phaidra at <a target=\"_blank\" href=\"http://phaidraservice.univie.ac.at/\">http://phaidraservice.univie.ac.at/</a>",
+  HELP_6: "For questions about Phaidra and PhaidraTemp, please contact <a href=\"mailto:phaidra@univie.ac.at\">phaidra@univie.ac.at</a>",
+  HELP_7: "For technical questions please contact the service address <a href=\"mailto:support.phaidra@univie.ac.at\">support.phaidra@univie.ac.at</a>."
+};
+ 
+var translationsDE= {  
+  DELETE: 'Löschen',
+  DELETE_BUTTON: 'Löschen',
+  BACK_BUTTON: 'Zurück',  
+  DELETE_TEXT: 'Sie finden hier alle Objekte, die Sie löschen möchten. Bitte beachten Sie, dass nach Betätigung des  Buttons „Löschen“ die Objekte gelöscht sind und nicht wiederhergestellt werden können.',
+  DELETE_SUCCESS: 'Ihre Objekte wurden nun wie gewünscht gelöscht.',
+  HELP: 'Hilfe',
+  CONTACT: 'Kontakt',
+  TITLE: 'Titel',
+  OBJECTS: 'Objekte',
+  HITS: 'Ergebnisse',
+  PHAIDRA_MANAGEMENTMODULE: 'Phaidra Managementmodul',
+  BUTTON_LANG_DE: 'Deutsch',
+  BUTTON_LANG_EN: 'English',
+  CONTACT_1: "Universität Wien<br/>Bibliotheks- und Archivwesen<br/>Universitätsring 1<br/>A-1010 Wien<br/>Österreich",
+  SERVICEPAGE: "Serviceseite",
+  IMPRESSUM_1: "Impressum",
+  IMPRESSUM_2: "Offenlegung gem. § 25 MedG - Verantwortlich für den Inhalt dieser Seite",
+  IMPRESSUM_3: "Phaidra übernimmt keine Haftung für den Inhalt verlinkter, “externer“ Webseiten.",
+  HELP_1: "Das Management-Tool  steht Ihnen für Folgende Aktivitäten zur Verfügung",
+  HELP_2: "1. Sie können Objekte aus PhaidraTemp löschen",
+  HELP_3: "2. Sie können Objekte aus PhaidraTemp in Phaidra langzeitarchivieren",
+  HELP_4: "Bitte beachten Sie vor der Löschung der Objekte, dass diese nach dem Löschvorgang nicht wiederhergestellt werden können. Bevor Sie Objekte archivieren, informieren Sie sich bitte über die Nutzungsbedingungen von Phaidra: <a target=\"_blank\" href=\"https://phaidra.univie.ac.at/terms_of_use/show_terms_of_use\">https://phaidra.univie.ac.at/terms_of_use/show_terms_of_use</a>",
+  HELP_5: "Weitere Informationen über Phaidra finden Sie unter <a target=\"_blank\" href=\"http://phaidraservice.univie.ac.at/\">http://phaidraservice.univie.ac.at/</a>",
+  HELP_6: "Bei Fragen zu Phaidra oder PhaidraTemp wenden Sie sich bitte an <a href=\"mailto:phaidra@univie.ac.at\">phaidra@univie.ac.at</a>",
+  HELP_7: "Bei technischen Fragen steht Ihnen die Serviceadresse <a href=\"mailto:support.phaidra@univie.ac.at\">support.phaidra@univie.ac.at</a> zur Verfügung."
+};
+
+app.config(['$translateProvider', function ($translateProvider) {
+  // add translation tables
+  $translateProvider.translations('en', translationsEN);
+  $translateProvider.translations('de', translationsDE);
+  $translateProvider.preferredLanguage('de');
+  $translateProvider.fallbackLanguage('de');
+  $translateProvider.useSanitizeValueStrategy('sanitize');
+}]);
 
 app.factory('pT', function (promiseTracker) {
   return promiseTracker();
 });
 
-app.run(function($rootScope, pT) {
-    $rootScope.spinnerOpts = {
+app.run(function($rootScope, $translate, $modal, pT) {
+
+  $rootScope.changeLanguage = function (langKey) {
+    $translate.use(langKey);
+  };
+
+  $rootScope.currentLanguage = function () {
+    return $translate.proposedLanguage();
+  };
+  
+  $rootScope.spinnerOpts = {
       /*
       lines: 8 // The number of lines to draw
     , length: 0 // The length of each line
@@ -58,7 +133,31 @@ app.run(function($rootScope, pT) {
     }
   });
 
+  $rootScope.contact_open = function () {
+    var modalInstance = $modal.open({
+      templateUrl: $('head base').attr('href')+'partials/modals/contact.html',
+      controller: InfoModalCtrl
+    });
+  };
+
+  $rootScope.help_open = function () {
+    var modalInstance = $modal.open({
+      templateUrl: $('head base').attr('href')+'partials/modals/help.html',
+      controller: InfoModalCtrl
+    });
+  };
+
+  $rootScope.impressum_open = function () {
+    var modalInstance = $modal.open({
+      templateUrl: $('head base').attr('href')+'partials/modals/impressum.html',
+      controller: InfoModalCtrl
+    });
+  };
+
 })
+
+
+
 
 app.config(['$routeProvider',
   function($routeProvider) {
@@ -191,7 +290,7 @@ actionControllers.controller('PushCtrl', function($scope, $routeParams, DataServ
   $scope.init();
 });
 
-actionControllers.controller('ListCtrl', function($rootScope, $scope, $window, $location, $modal, $log, FrontendService, DataService, pT) {
+actionControllers.controller('ListCtrl', function($rootScope, $scope, $window, $location, $modal, $log, $translate, FrontendService, DataService, pT) {
   
   $scope.alerts = {
     "phaidra-temp": [],
@@ -460,6 +559,12 @@ var SigninModalCtrl = function ($scope, $modalInstance, FrontendService, pT) {
 
 var AlertModalCtrl = function ($scope, $modalInstance, msg) {
   $scope.msg = msg;
+  $scope.ok = function () {
+    $modalInstance.dismiss('ok');
+  };
+};
+
+var InfoModalCtrl = function ($scope, $modalInstance) {  
   $scope.ok = function () {
     $modalInstance.dismiss('ok');
   };
